acfa0cb3d38fe823ba1ff80174cbc6aa0f6f77de
hbahk <bahkhyeonguk@gmail.com>
2022-05-21 14:35:04 +0900

    changed apostrophes to quotation marks

--- exgalcosutils/catalog.py
+++ exgalcosutils/catalog.py
@@ -11,10 +11,10 @@ from matplotlib import pyplot as plt
 from astropy.coordinates import SkyCoord
 from astropy import unit as u
 
-__all__ = ['match_catalogs', 'plot_offset_dist']
+__all__ = ["match_catalogs", "plot_offset_dist"]
 
 def match_catalogs(cat_lowres, cat_highres, tol):
-    '''
+    """
     Cross-match two catalogs from different astronomical surveys, based on
     their celestrial positions.
     
@@ -54,8 +54,8 @@ def match_catalogs(cat_lowres, cat_highres, tol):
         
     >>> from astropy.coordinates import SkyCoord
     >>> from astropy import units as u
-    >>> psz_coord = SkyCoord(ra=psz['RAdeg'], dec=psz['DEdeg'], unit='deg')
-    >>> xcls_coord = SkyCoord(ra=xcls['RAdeg'], dec=xcls'DEdeg'], unit='deg')
+    >>> psz_coord = SkyCoord(ra=psz["RAdeg"], dec=psz["DEdeg"], unit="deg")
+    >>> xcls_coord = SkyCoord(ra=xcls["RAdeg"], dec=xcls"DEdeg"], unit="deg")
     
     Then we can carry out positional catalog matching:
         
@@ -67,19 +67,19 @@ def match_catalogs(cat_lowres, cat_highres, tol):
     Planck data is ~ 7 arcmin. The `tol` parameter were set to be 7 
     arcmin, since it is apparently larger than 6 arcsec.
     
-    '''
+    """
     
     if not (isinstance(cat_lowres, SkyCoord)
             and isinstance(cat_highres, SkyCoord)):
-        raise ValueError('`cat_lowres` and `cat_highres` should be '+
-                         'SkyCoord objects')
+        raise ValueError("`cat_lowres` and `cat_highres` should be "+
+                         "SkyCoord objects")
     
     if not isinstance(tol, u.quantity.Quantity):
         if type(tol) is float or type(tol) is int:
             tol = tol * u.arcsec
         else:
-            raise ValueError('`tol` should be either a number (int or float)'+
-                             ' or a Quantity object with given unit')
+            raise ValueError("`tol` should be either a number (int or float)"+
+                             " or a Quantity object with given unit")
     
     # save original order
     ih = np.arange(len(cat_highres))    # original index of cat_highres
@@ -95,7 +95,7 @@ def match_catalogs(cat_lowres, cat_highres, tol):
     idxh = ih[idx_unique]   # matched index of cat_highres
     
     # 2nd match with pre-matched catalog where all duplications were ruled out
-    # cmil ('c'ross 'm'atched 'i'ndex for 'l'ow resolution catalog)
+    # cmil ("c"ross "m"atched "i"ndex for "l"ow resolution catalog)
     cmil, _, _ = cat_highres_matched.match_to_catalog_sky(cat_lowres)
 
     idxl = il[cmil]    # matched index of cat_lowres
@@ -105,7 +105,7 @@ def match_catalogs(cat_lowres, cat_highres, tol):
 
 def plot_offset_dist(c1, c2, tol, hist=False, plot_kwargs={}, hist_kwargs={},
                      circ_kwargs={}, text_kwargs={}, return_axes=False):
-    '''
+    """
     Plot a offset distribution figure for positions of two matched catalogs.
 
     Parameters
@@ -141,42 +141,42 @@ def plot_offset_dist(c1, c2, tol, hist=False, plot_kwargs={}, hist_kwargs={},
     -------
     (Optional) AxesSubplot or array of AxesSubplot, if return_axis=True
 
-    '''
+    """
     if not (isinstance(c1, SkyCoord) and isinstance(c2, SkyCoord)):
-        raise ValueError('`c1` and `c2` should be SkyCoord objects')
+        raise ValueError("`c1` and `c2` should be SkyCoord objects")
     
     if not isinstance(tol, u.quantity.Quantity):
         if type(tol) is float or type(tol) is int:
             tol = tol * u.arcsec
         else:
-            raise ValueError('`tol` should be either a number (int or float)'+
-                             ' or a Quantity object with given unit')
+            raise ValueError("`tol` should be either a number (int or float)"+
+                             " or a Quantity object with given unit")
     
     dra, ddec = c2.spherical_offsets_to(c1)
     dra, ddec = dra.to(tol.unit), ddec.to(tol.unit)
     
     if hist:
         fig, axes = plt.subplots(2, 2, figsize=(7,7),
-                                 gridspec_kw={'width_ratios': [4, 1],
-                                              'height_ratios': [1, 4]})
+                                 gridspec_kw={"width_ratios": [4, 1],
+                                              "height_ratios": [1, 4]})
         (hhax, nax), (ax, vhax) = axes
     else:
         fig, ax = plt.subplots(1, 1, figsize=(7,7))
     
     # main scatter plot
-    # - default kwargs setting (fmt='.', c='k')
-    fmt = plot_kwargs.pop('fmt', '.')
-    color_keyname = 'c' if 'c' in plot_kwargs.keys() else 'color' # for alias
-    plot_c = plot_kwargs.pop(color_keyname, 'k')
+    # - default kwargs setting (fmt=".", c="k")
+    fmt = plot_kwargs.pop("fmt", ".")
+    color_keyname = "c" if "c" in plot_kwargs.keys() else "color" # for alias
+    plot_c = plot_kwargs.pop(color_keyname, "k")
     
     # - plotting positional offset
     ax.plot(dra.value, ddec.value, fmt=fmt, c=plot_c, **plot_kwargs)
     
-    # - default kwargs for the tolerance circle (fill=False, ec='k', zorder=0)
-    circ_fill = circ_kwargs.pop('fill', False)
-    ec_keyname = 'ec' if 'ec' in circ_kwargs.keys() else 'edgecolor'
-    circ_ec = circ_kwargs.pop(ec_keyname, 'k')
-    circ_zorder = circ_kwargs.pop('zorder', 0)
+    # - default kwargs for the tolerance circle (fill=False, ec="k", zorder=0)
+    circ_fill = circ_kwargs.pop("fill", False)
+    ec_keyname = "ec" if "ec" in circ_kwargs.keys() else "edgecolor"
+    circ_ec = circ_kwargs.pop(ec_keyname, "k")
+    circ_zorder = circ_kwargs.pop("zorder", 0)
     
     # - drawing the circle
     circ = plt.Circle((0,0), tol.value, fill=circ_fill, ec=circ_ec,
@@ -184,53 +184,53 @@ def plot_offset_dist(c1, c2, tol, hist=False, plot_kwargs={}, hist_kwargs={},
     ax.add_patch(circ)
     
     # - making x and y scale equal
-    # ax.set_aspect('equal') # This doesn't work well
+    # ax.set_aspect("equal") # This doesn"t work well
     xlim, ylim = ax.get_xlim(), ax.get_ylim()
     lim = np.array([-1,1]) * np.max(np.abs(np.concatenate((xlim, ylim))))
     ax.set_xlim(lim)
     ax.set_ylim(lim)
     
     # - horizontal and vertical line passing the zeropoint
-    ax.axhline(0, c='k', ls=':', lw=1)
-    ax.axvline(0, c='k', ls=':', lw=1)
+    ax.axhline(0, c="k", ls=":", lw=1)
+    ax.axvline(0, c="k", ls=":", lw=1)
     
     # - default kwargs for the text
-    # +- (x=0.99, y=0.01, va='bottom', ha='right', transform=ax.transAxes)
-    text_x, text_y = text_kwargs.pop('x', 0.99), text_kwargs.pop('y', 0.01)
-    va_keyname = 'va' if 'va' in text_kwargs.keys() else 'verticalalignment'
-    ha_keyname = 'ha' if 'ha' in text_kwargs.keys() else 'horizontalignment'
-    text_va = text_kwargs.pop(va_keyname,'bottom')
-    text_ha = text_kwargs.pop(ha_keyname, 'right')
-    text_transform = text_kwargs.pop('transform', ax.transAxes)
+    # +- (x=0.99, y=0.01, va="bottom", ha="right", transform=ax.transAxes)
+    text_x, text_y = text_kwargs.pop("x", 0.99), text_kwargs.pop("y", 0.01)
+    va_keyname = "va" if "va" in text_kwargs.keys() else "verticalalignment"
+    ha_keyname = "ha" if "ha" in text_kwargs.keys() else "horizontalignment"
+    text_va = text_kwargs.pop(va_keyname,"bottom")
+    text_ha = text_kwargs.pop(ha_keyname, "right")
+    text_transform = text_kwargs.pop("transform", ax.transAxes)
     
     # - add information about the tolerance
-    ax.text(text_x, text_y, f'TOL={tol:.1f}',
+    ax.text(text_x, text_y, f"TOL={tol:.1f}",
             va=text_va, ha=text_ha, transform=text_transform, **text_kwargs)
     
     # - axis label
-    ax.set_xlabel(r'$\Delta \alpha$ ' + f'[{tol.unit.name}]')
-    ax.set_ylabel(r'$\Delta \delta$ ' + f'[{tol.unit.name}]')
+    ax.set_xlabel(r"$\Delta \alpha$ " + f"[{tol.unit.name}]")
+    ax.set_ylabel(r"$\Delta \delta$ " + f"[{tol.unit.name}]")
     
     # - changing tick direction and adding minor ticks
     ax.minorticks_on()
-    ax.tick_params(direction='in', top=True, right=True)
-    ax.tick_params(which='minor', direction='in', top=True, right=True)
+    ax.tick_params(direction="in", top=True, right=True)
+    ax.tick_params(which="minor", direction="in", top=True, right=True)
     
     # histograms on side axes
     if hist:
         # - unuse top-right axis
-        nax.axis('off')
+        nax.axis("off")
         
-        # - default kwargs for histogram (bins=20, histtype='step', color='k')
-        bins = hist_kwargs.pop('bins', 20)
-        histtype = hist_kwargs.pop('histtype', 'step')
-        hist_color = hist_kwargs.pop('color', 'k')
+        # - default kwargs for histogram (bins=20, histtype="step", color="k")
+        bins = hist_kwargs.pop("bins", 20)
+        histtype = hist_kwargs.pop("histtype", "step")
+        hist_color = hist_kwargs.pop("color", "k")
         
         # - drawing histogram
         _ = hhax.hist(dra.value, bins=bins, histtype=histtype,
                       color=hist_color, **hist_kwargs)
         _ = vhax.hist(ddec.value, bins=bins, histtype=histtype,
-                      color=hist_color, orientation='horizontal',
+                      color=hist_color, orientation="horizontal",
                       **hist_kwargs)
         
         hhax.get_shared_x_axes().join(ax, hhax)
@@ -242,9 +242,9 @@ def plot_offset_dist(c1, c2, tol, hist=False, plot_kwargs={}, hist_kwargs={},
         for axis, lb, ll in zip([hhax, vhax], [False, True], [True, False]):
 
             axis.minorticks_on()
-            axis.tick_params(direction='in', top=True, right=True,
+            axis.tick_params(direction="in", top=True, right=True,
                              labelbottom=lb, labelleft=ll)
-            axis.tick_params(which='minor', direction='in',
+            axis.tick_params(which="minor", direction="in",
                              top=True, right=True)
             
             plt.draw()    # to populate tick labels
