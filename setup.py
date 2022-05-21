4cde1f89badf63a26a2737c38ed4cd9b16af747e
hbahk <bahkhyeonguk@gmail.com>
2022-05-21 14:23:50 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptils import setup, find_packages
+
+setup_requires = []
+install_requires = [
+    'numpy',
+    'matplotlib',
+    'astropy'
+]
+
+setup(name='exgalcosutils',
+      version='0.1.dev',
+      url='https://github.com/hbahk/exgalcosutils',
+      author='Hyeonguk Bahk',
+      author_email='hyeongukbahk@gmail.com',
+      description='Utilities for astronomical data handling and data analysis',
+      packages=find_packages(),
+      long_description=open('README.md').read(),
+      zip_safe=False,
+      setup_requires=setup_requires,
+      install_requires=install_requires
+      )
