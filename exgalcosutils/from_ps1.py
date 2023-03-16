d8dc13f05237bc6c21e187d3465f4b7a12cb3469
hbahk <bahkhyeonguk@gmail.com>
2023-03-16 15:52:33 +0900

    ps1 image cutout

---
+++ exgalcosutils/from_ps1.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 14:14:31 2023
+
+@author: Hyeonguk Bahk
+
+https://ps1images.stsci.edu/ps1image.html
+"""
+
+import numpy as np
+from astropy.table import Table
+from astropy import units as u
+from astropy.io import fits
+from astropy.wcs import WCS
+import requests
+from PIL import Image
+from io import BytesIO
+from matplotlib import pyplot as plt
+
+from exgalcosutils.from_legacy_survey import dr2_rgb, sdss_rgb
+
+
+def get_ps1_image(cra, cdec, size=240, output_size=None):
+    if type(size) == u.quantity.Quantity:
+        SCALE = 0.25*u.arcsec  # scale angle of a pixel
+        pix_len = round(((size/SCALE).decompose().value))
+    elif type(size) == int:
+        pix_len = size
+    else:
+        raise ValueError('`size` should be either type of int '+
+                         'astropy Quantity in angular unit')
+    url = geturl(cra, cdec, size=pix_len, filters='i', format='fits',
+                 output_size=output_size)
+    hdu = fits.open(url[0])
+    wcs = WCS(hdu[0].header)
+    
+    rgbimg = getcolorim(cra, cdec, size=size, output_size=output_size,
+                        filters="grz", format="jpg")
+    
+    return rgbimg, wcs
+
+def getimages(ra,dec,filters="grizy"):
+    
+    """Query ps1filenames.py service to get a list of images
+    
+    ra, dec = position in degrees
+    size = image size in pixels (0.25 arcsec/pixel)
+    filters = string with filters to include
+    Returns a table with the results
+    """
+    
+    service = "https://ps1images.stsci.edu/cgi-bin/ps1filenames.py"
+    url = f"{service}?ra={ra}&dec={dec}&filters={filters}"
+    table = Table.read(url, format='ascii')
+    return table
+
+
+def geturl(ra, dec, size=240, output_size=None, filters="grizy", format="jpg", color=False):
+    
+    """Get URL for images in the table
+    
+    ra, dec = position in degrees
+    size = extracted image size in pixels (0.25 arcsec/pixel)
+    output_size = output (display) image size in pixels (default = size).
+                  output_size has no effect for fits format images.
+    filters = string with filters to include
+    format = data format (options are "jpg", "png" or "fits")
+    color = if True, creates a color image (only for jpg or png format).
+            Default is return a list of URLs for single-filter grayscale images.
+    Returns a string with the URL
+    """
+    
+    if color and format == "fits":
+        raise ValueError("color images are available only for jpg or png formats")
+    if format not in ("jpg","png","fits"):
+        raise ValueError("format must be one of jpg, png, fits")
+    table = getimages(ra,dec,filters=filters)
+    url = (f"https://ps1images.stsci.edu/cgi-bin/fitscut.cgi?"
+           f"ra={ra}&dec={dec}&size={size}&format={format}")
+    if output_size:
+        url = url + "&output_size={}".format(output_size)
+    # sort filters from red to blue
+    flist = ["yzirg".find(x) for x in table['filter']]
+    table = table[np.argsort(flist)]
+    if color:
+        if len(table) > 3:
+            # pick 3 filters
+            table = table[[0,len(table)//2,len(table)-1]]
+        for i, param in enumerate(["red","green","blue"]):
+            url = url + "&{}={}".format(param,table['filename'][i])
+    else:
+        urlbase = url + "&red="
+        url = []
+        for filename in table['filename']:
+            url.append(urlbase+filename)
+    return url
+
+
+def getcolorim(ra, dec, size=240, output_size=None, filters="grizy", format="jpg"):
+    
+    """Get color image at a sky position
+    
+    ra, dec = position in degrees
+    size = extracted image size in pixels (0.25 arcsec/pixel)
+    output_size = output (display) image size in pixels (default = size).
+                  output_size has no effect for fits format images.
+    filters = string with filters to include
+    format = data format (options are "jpg", "png")
+    Returns the image
+    """
+    
+    if format not in ("jpg","png"):
+        raise ValueError("format must be jpg or png")
+    url = geturl(ra,dec,size=size,filters=filters,output_size=output_size,format=format,color=True)
+    r = requests.get(url)
+    im = Image.open(BytesIO(r.content))
+    return im
+
+
+def getgrayim(ra, dec, size=240, output_size=None, filter="g", format="jpg"):
+    
+    """Get grayscale image at a sky position
+    
+    ra, dec = position in degrees
+    size = extracted image size in pixels (0.25 arcsec/pixel)
+    output_size = output (display) image size in pixels (default = size).
+                  output_size has no effect for fits format images.
+    filter = string with filter to extract (one of grizy)
+    format = data format (options are "jpg", "png")
+    Returns the image
+    """
+    
+    if format not in ("jpg","png"):
+        raise ValueError("format must be jpg or png")
+    if filter not in list("grizy"):
+        raise ValueError("filter must be one of grizy")
+    url = geturl(ra,dec,size=size,filters=filter,output_size=output_size,format=format)
+    r = requests.get(url[0])
+    im = Image.open(BytesIO(r.content))
+    return im
